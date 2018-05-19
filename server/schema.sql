204889d021dda5a98b03f1a8b6212faecb6cc5e1
jehwa <s.jehwa@gmail.com>
2018-05-18 18:43:25 -0700

    Edited tables for database

--- server/schema.sql
+++ server/schema.sql
@@ -2,11 +2,34 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
+CREATE TABLE Users (
+  Id int primary key,
+  User_Name varchar(100)
+);
+
+CREATE TABLE Rooms (
+  Id int primary key,
+  Room_Name varchar(100)
+);
+
+CREATE TABLE Messages (
+  Id int primary key, 
+  Message varchar(500), 
+  User int,
+  Room int,
+  foreign key(User) references Users(Id), 
+  foreign key(Room) references Rooms(Id)
   /* Describe your table here.*/
 );
 
 /* Create other tables and define schemas for them here! */
+
+CREATE TABLE Users_Rooms (
+  UserId int,
+  RoomId int,
+  foreign key(UserId) references Users(Id),
+  foreign key(RoomId) references Rooms(Id)
+);
 
 
 
