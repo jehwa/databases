ce286fb98e64f00c794dffe4eaa810c5987a7bba
jehwa <s.jehwa@gmail.com>
2018-05-18 21:29:37 -0700

    WIP - server is seeing the data

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,15 +1,26 @@
 var models = require('../models');
+var app = require('../app')
 
 module.exports = {
   messages: {
-    get: function (req, res) {}, // a function which handles a get request for all messages
-    post: function (req, res) {} // a function which handles posting a message to the database
+    get: function (req, res) {
+      //pass the data to model. 
+      // console.log(req, '<------------controllers-------------')
+    }, // a function which handles a get request for all messages
+    post: function (req, res) {
+      // models.messages.post(req, res);
+      // console.log(req, '<----------------------------')
+    } // a function which handles posting a message to the database
   },
 
   users: {
     // Ditto as above
     get: function (req, res) {},
-    post: function (req, res) {}
+    post: function (req, res) {
+      models.users.get(req.body);
+      // console.log(req.body);
+     
+    }
   }
 };
 
--- server/models/index.js
+++ server/models/index.js
@@ -2,13 +2,17 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
+    get: function (req, res) {
+      db.
+    }, // a function which produces all the messages
     post: function () {} // a function which can be used to insert a message into the database
   },
 
   users: {
     // Ditto as above.
-    get: function () {},
+    get: function (body) {
+      
+    },
     post: function () {}
   }
 };
